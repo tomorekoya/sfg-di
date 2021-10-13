09d9df3c7f459889ace2a073e02fb8a227f8eb03
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2021-10-13 08:13:06 -0400

    Initial commit.

---
+++ src/test/java/com/triddle/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triddle.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
