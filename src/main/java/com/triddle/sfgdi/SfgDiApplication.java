c68cb19b4a1ef8be14c567fe7227c77920be8528
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2021-10-15 13:15:52 -0400

    37 - Primary Beans

--- src/main/java/com/triddle/sfgdi/SfgDiApplication.java
+++ src/main/java/com/triddle/sfgdi/SfgDiApplication.java
@@ -14,10 +14,10 @@ public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-        MyController myController = (MyController) ctx.getBean("myController");
+        System.out.println("---- Primary Bean ----");
 
-        String greeting = myController.sayHello();
-        System.out.println(greeting);
+        MyController myController = (MyController) ctx.getBean("myController");
+        System.out.println(myController.sayHello());
 
         System.out.println("---- Property ----");
 
