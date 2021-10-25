119d4db5fa3904f431f7b597d606c692c01c1ae2
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2021-10-25 10:39:04 -0400

    39 - Default Profile

--- src/main/java/com/triddle/sfgdi/services/I18NSpanishService.java
+++ src/main/java/com/triddle/sfgdi/services/I18NSpanishService.java
@@ -3,7 +3,7 @@ package com.triddle.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
 
